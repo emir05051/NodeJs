a0130a4662496481acfa86b78537984238dcad35
emir <emir05051@gmail.com>
2021-07-10 02:53:58 +0600

    master

---
+++ middlewares/update.middleware.js
@@ -0,0 +1,15 @@
+import { list } from "../controllers/createTask.js";
+
+export default (req, res, next) => {
+  try {
+    if (list.length == 0) {
+      throw new Error(
+        res.status(401).json({ message: "Вы не добавили ни единого задания" })
+      );
+    }
+    console.log("Вышел");
+    next();
+  } catch (e) {
+    console.log(e);
+  }
+};
